8a5a65c165b975b1c93d8187b3652bae7cb745ee
fengzhongshuang <fengzhongshuang@bytedance.com>
2019-09-20 10:10:44 +0800

    240
    Change-Id: I1e94044871e54f7ed7980f8dfe7acc430a419c02

---
+++ 240.py
@@ -0,0 +1,62 @@
+# coding=utf8
+# 240. 搜索二维矩阵 II
+# 编写一个高效的算法来搜索 m x n 矩阵 matrix 中的一个目标值 target。该矩阵具有以下特性：
+# 每行的元素从左到右升序排列。
+# 每列的元素从上到下升序排列。
+# 示例:
+# 现有矩阵 matrix 如下：
+# [
+#     [1,   4,  7, 11, 15],
+#     [2,   5,  8, 12, 19],
+#     [3,   6,  9, 16, 22],
+#     [10, 13, 14, 17, 24],
+#     [18, 21, 23, 26, 30]
+# ]
+# 给定 target = 5，返回 true。
+# 给定 target = 20，返回 false。
+
+
+class Solution(object):
+    def searchMatrix(self, matrix, target):
+        """
+        :type matrix: List[List[int]]
+        :type target: int
+        :rtype: bool
+        """
+        if not matrix:
+            return False
+        rows = len(matrix)
+        if not matrix[0]:
+            return False
+        cols = len(matrix[0]) - 1
+        for i in range(rows):
+            while cols >= 0:
+                if matrix[i][cols] == target:
+                    return True
+                elif matrix[i][cols] > target:
+                    cols -= 1
+                else:
+                    break
+        return False
+
+
+s = Solution()
+print s.searchMatrix([
+    [1,   4,  7, 11, 15],
+    [2,   5,  8, 12, 19],
+    [3,   6,  9, 16, 22],
+    [10, 13, 14, 17, 24],
+    [18, 21, 23, 26, 30]
+], 5)
+
+print s.searchMatrix([
+    [1,   4,  7, 11, 15],
+    [2,   5,  8, 12, 19],
+    [3,   6,  9, 16, 22],
+    [10, 13, 14, 17, 24],
+    [18, 21, 23, 26, 30]
+], 20)
+
+print s.searchMatrix([
+    [-5]
+], -5)
