960c15134e13e8be5b6c9d34f87db02f8c58b297
fengzhongshuang <fengzhongshuang@bytedance.com>
2019-09-20 09:37:15 +0800

    169
    Change-Id: Iae9461a9f91f473f29074f100881f2c999ddcac6

---
+++ 169.py
@@ -0,0 +1,27 @@
+# coding=utf8
+
+# 169. 求众数
+# 给定一个大小为 n 的数组，找到其中的众数。众数是指在数组中出现次数大于 ⌊ n/2 ⌋ 的元素。
+# 你可以假设数组是非空的，并且给定的数组总是存在众数。
+# 示例 1:
+# 输入: [3, 2, 3]
+# 输出: 3
+# 示例 2:
+# 输入: [2, 2, 1, 1, 1, 2, 2]
+# 输出: 2
+
+
+class Solution(object):
+    def majorityElement(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        nums = sorted(nums)
+        mid = len(nums) // 2
+        return nums[mid]
+
+
+s = Solution()
+print s.majorityElement([3, 2, 3])
+print s.majorityElement([2, 2, 1, 1, 1, 2, 2])
