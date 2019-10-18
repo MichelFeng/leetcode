556b9ef2ac4feba0717b55fee988c7dc1a6c982e
fengzhongshuang <fengzhongshuang@bytedance.com>
2019-10-18 14:01:27 +0800

    141.py
    Change-Id: I08e61b0239e4b30734c0f896abc3206e313cd784

---
+++ 141.py
@@ -0,0 +1,26 @@
+# coding=utf8
+# 141. 环形链表
+# 给定一个链表，判断链表中是否有环。
+# 为了表示给定链表中的环，我们使用整数 pos 来表示链表尾连接到链表中的位置（索引从 0 开始）。 如果 pos 是 - 1，则在该链表中没有环。
+
+
+class ListNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+
+class Solution(object):
+    def hasCycle(self, head):
+        """
+        :type head: ListNode
+        :rtype: bool
+        """
+        visited = set()
+        while head:
+            if head in visited:
+                return True
+            else:
+                visited.add(head)
+                head = head.next
+        return False
